23b3590a12af58901d6fa915b4fd3129b8ce6d9e
suvamneog <neogsuvam@gmail.com>
2025-04-10 14:36:42 +0530

    UPDATED UI

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get("/projects", (req, res) => {
-  res.render("projects.ejs"); });
+// app.get("/projects", (req, res) => {
+//   res.render("projects.ejs"); });
 
 // // Handle form submissions
 // app.post('/submit-form', (req, res) => {
